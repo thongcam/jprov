a2fb4dc46d1919f4f925619c7164217815c9efaa
Thong Cam <thongcam02@gmail.com>
2022-01-20 10:16:21 +0200

    Add filtering function

--- recommendations-and-pre-loved-practices/index.js
+++ recommendations-and-pre-loved-practices/index.js
@@ -5,3 +5,38 @@ $(".help-button").click((event) => {
 $(".close-button").click((event) => {
   $(event.target).parents(".help-box").toggleClass("show");
 });
+
+$(".filter-checkbox input[type='checkbox']").change((event) => {
+  let filterMap = {};
+
+  $(".filters-wrapper input[type='checkbox']").each(
+    (i, elem) => (filterMap[elem.name] = [])
+  );
+
+  $(event.target)
+    .parents(".filters-wrapper")
+    .find("input[type='checkbox']")
+    .each((i, elem) => {
+      if (elem.checked) {
+        filterMap[elem.name].push(elem.value);
+      }
+    });
+
+  $(event.target)
+    .parents(".filters-cards-wrapper")
+    .find(".item-card")
+    .each((i, card) => {
+      let showCard = true;
+      Object.keys(filterMap).forEach((key, i) => {
+        if (!filterMap[key].includes($(card).data(key))) {
+          showCard = false;
+        }
+      });
+
+      if (showCard) {
+        $(card).addClass("show");
+      } else {
+        $(card).removeClass("show");
+      }
+    });
+});
